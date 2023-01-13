d266dcccff3e0f01664d97e8dfeaf55e9569d484
William França <will.franca1998@gmail.com>
2023-01-13 20:41:25 -0300

    modified:   css/responsive.css 	modified:   index.html 	modified:   js/events.js 	modified:   style.css

--- js/events.js
+++ js/events.js
@@ -2,13 +2,24 @@ $ = document.querySelector.bind(document);
 $$ = document.querySelectorAll.bind(document); 
 print = console.log.bind(console)
 
-const ingresso = document.querySelector(".ingresso");
+/* const ingresso = document.querySelector(".ingresso");
 
 let ingressoLargura = (ingresso).clientWidth
-let ingressoAltura = (ingresso).clientHeight
+let ingressoAltura = (ingresso).clientHeight */
 
-print(ingressoLargura, ingressoAltura)
+/* print(ingressoLargura, ingressoAltura) */
 
-let boxSection = document.getElementById("box-section");
-boxSection.style.height = ingressoAltura;
+/* let boxSection = document.getElementById("box-section");
+boxSection.style.height = ingressoAltura; */
 
+var boxSection = document.getElementById("box-section");
+var ingresso = document.getElementById("ingresso-1");
+
+if(document.body.clientWidth < 900){
+    boxSection.style.height = ((ingresso.offsetHeight*2+27) + "px");
+}
+if(document.body.clientWidth > 900){
+    boxSection.style.height = ((ingresso.offsetHeight+20) + "px");
+}
+
+print(boxSection.clientHeight, ingresso.clientHeight)
