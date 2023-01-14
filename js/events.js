da476090ab12ffe1cba5162a61ac43537ed1ac26
William França <will.franca1998@gmail.com>
2023-01-14 09:27:34 -0300

    modified:   index.html 	modified:   js/events.js 	modified:   style.css

--- js/events.js
+++ js/events.js
@@ -2,20 +2,20 @@ $ = document.querySelector.bind(document);
 $$ = document.querySelectorAll.bind(document);
 print = console.log.bind(console)
 
-/* const ingresso = document.querySelector(".ingresso");
 
-let ingressoLargura = (ingresso).clientWidth
-let ingressoAltura = (ingresso).clientHeight */
-
-/* print(ingressoLargura, ingressoAltura) */
-
-/* let boxSection = document.getElementById("box-section");
-boxSection.style.height = ingressoAltura; */
+/* Procurar o height da div.ingresso e ajustar a div#box-section com base neste valor. Transformando o resultado em pixels*/
 
 var boxSection = document.getElementById("box-section");
 var ingresso = document.getElementById("ingresso-1");
 
-document.body.onresize = function () {
+if (document.body.clientWidth < 900) {  /* ajusta o valor da var ingresso ao carregar a página */
+    boxSection.style.height = ((ingresso.offsetHeight * 2 + 27) + "px");
+}
+else {
+    boxSection.style.height = ((ingresso.offsetHeight + 20) + "px");
+}
+
+document.body.onresize = function () { /* ajusta o valor da var ingresso ao redimensionar a página */
     if (document.body.clientWidth < 900) {
         boxSection.style.height = ((ingresso.offsetHeight * 2 + 27) + "px");
     }
@@ -23,21 +23,17 @@ document.body.onresize = function () {
         boxSection.style.height = ((ingresso.offsetHeight + 20) + "px");
     }
 };
-if (document.body.clientWidth < 900) {
-    boxSection.style.height = ((ingresso.offsetHeight * 2 + 27) + "px");
-}
-else {
-    boxSection.style.height = ((ingresso.offsetHeight + 20) + "px");
-}
 
-if (document.body.clientWidth < 900) {
+
+if (document.body.clientWidth < 900) { /* Procurar o height da div.ingresso em valor, sem transformar em pixels com base no tamanho da tela*/
     var tamanhoIngresso = (ingresso.offsetHeight * 2 + 25);
 }
 else {
     var tamanhoIngresso = (ingresso.offsetHeight + 15);
 }
 
-var button = document.getElementById("wrapper2");
+
+var button = document.getElementById("wrapper2"); /* botão "Ver mais / Ver menos" */
 
 var height = (tamanhoIngresso); // valor inicial da altura
 var expanded = false; // flag para verificar se o botão está expandido ou não
